3b586b798c33e8e7b65a97cfb149b2bb8e5c3f5e
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-27 10:18:12 -0300

    feat: lesson 6 - Adding a button

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -34,10 +35,7 @@ fun MyApp(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
-    Column(
-        modifier = modifier.padding(vertical = 4.dp)
-
-        ) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
             Greeting(name = name)
         }
@@ -47,18 +45,21 @@ fun MyApp(
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
-        color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(
-            horizontal = 8.dp, vertical = 4.dp
-        )
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = modifier
-            .padding(24.dp)
-            .fillMaxWidth()) {
-            Text(text = "Hello")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            ElevatedButton( onClick = { /* TODO */}) {
+                Text("Show More")
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
