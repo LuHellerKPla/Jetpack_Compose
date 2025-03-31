c7b50a3cfde6fbecdb3ccba002f9af0fe03aba10
LuizaHellerKPla <luiza.pla@poatek.com>
2025-03-31 11:47:55 -0300

    Setup - images

--- app/src/main/java/com/example/composebasics/MainActivity.kt
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -1,138 +1,201 @@
 package com.example.composebasics
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.composebasics.ui.theme.ComposeBasicsTheme
+import com.example.composebasics.ui.theme.MySootheTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ComposeBasicsTheme {
-                MyApp()
-            }
-        }
+        setContent { MySootheApp() }
     }
 }
 
+// Step: Search bar - Modifiers
 @Composable
-fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
-
-    if (shouldShowOnboarding) {
-        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false})
-    } else {
-        Greetings()
-    }
-}
-
-@Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Surface {
-        Column(modifier = Modifier.padding(vertical = 4.dp)) {
-            for (name in names) {
-                Greeting(name)
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPedding = if (expanded) 48.dp else 0.dp
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = modifier
-                    .weight(1f)
-                    .padding(bottom = extraPedding)
-            ) {
-                Text(text = "Hello,")
-                Text(text = name)
-            }
-            OutlinedButton(
-                onClick = { expanded = !expanded },
-                shape = MaterialTheme.shapes.small,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color.White,
-                    contentColor = Color.Black
-                )
-            ) {
-                Text(if (expanded) "Show less" else "Show more")
-            }
-        }
-    }
-}
-
-@Composable
-fun OnboardingScreen(
-    modifier: Modifier = Modifier,
-    onContinueClicked: () -> Unit
+fun SearchBar(
+    modifier: Modifier = Modifier
 ) {
-    // TODO: This state should be hoisted
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    // Implement composable here
+}
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Welcome to the Basics Codelab!")
-        Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
-        ) {
-            Text("Continue")
-        }
+// Step: Align your body - Alignment
+@Composable
+fun AlignYourBodyElement(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Favorite collection card - Material Surface
+@Composable
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Align your body row - Arrangements
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Favorite collections grid - LazyGrid
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Home section - Slot APIs
+@Composable
+fun HomeSection(
+    modifier: Modifier = Modifier
+) {
+    // Implement composable here
+}
+
+// Step: Home screen - Scrolling
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    // Implement composable here
+}
+
+// Step: Bottom navigation - Material
+@Composable
+private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+    // Implement composable here
+}
+
+// Step: MySoothe App - Scaffold
+@Composable
+fun MySootheAppPortrait() {
+    // Implement composable here
+}
+
+// Step: Bottom navigation - Material
+@Composable
+private fun SootheNavigationRail(modifier: Modifier = Modifier) {
+    // Implement composable here
+}
+
+// Step: Landscape Mode
+@Composable
+fun MySootheAppLandscape(){
+    // Implement composable here
+}
+
+// Step: MySoothe App
+@Composable
+fun MySootheApp() {
+    // Implement composable here
+}
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun SearchBarPreview() {
+    MySootheTheme { SearchBar(Modifier.padding(8.dp)) }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyElementPreview() {
+    MySootheTheme {
+        AlignYourBodyElement(
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320, uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
-fun OnboardingPreview() {
-    ComposeBasicsTheme {
-        OnboardingScreen(onContinueClicked = {})
+fun FavoriteCollectionCardPreview() {
+    MySootheTheme {
+        FavoriteCollectionCard(
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
 @Composable
-fun GreetingPreview() {
-    ComposeBasicsTheme {
-        MyApp()
-    }
+fun FavoriteCollectionsGridPreview() {
+    MySootheTheme { FavoriteCollectionsGrid() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun AlignYourBodyRowPreview() {
+    MySootheTheme { AlignYourBodyRow() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun HomeSectionPreview() {
+    MySootheTheme { HomeSection() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun ScreenContentPreview() {
+    MySootheTheme { HomeScreen() }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun BottomNavigationPreview() {
+    MySootheTheme { SootheBottomNavigation(Modifier.padding(top = 24.dp)) }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun NavigationRailPreview() {
+    MySootheTheme { SootheNavigationRail() }
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun MySoothePortraitPreview() {
+    MySootheAppPortrait()
+}
+
+@Preview(widthDp = 640, heightDp = 360)
+@Composable
+fun MySootheLandscapePreview() {
+    MySootheAppLandscape()
 }
