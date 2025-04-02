7f3936158a7e1cc4afe67b17a1875da0437fac61
LuizaHellerKPla <luiza.pla@poatek.com>
2025-04-02 14:11:20 -0300

    feat: lessons 6-7

--- app/src/main/java/com/example/composebasics/WaterCounter.kt
+++ app/src/main/java/com/example/composebasics/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.composebasics
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -17,9 +18,32 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         var count by remember { mutableStateOf(0) }
 
-        Text("You've had $count glasses.")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text("Add One")
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            //This text is present if the button has been clicked at least once;
+            // absent otherwise.
+            Text("You've had $count glasses of water.")
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(
+                onClick = { count++ },
+                enabled = count < 10
+            ) {
+                Text("Add One")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)
+            ) {
+                Text("Clear water count")
+            }
+
         }
     }
 
